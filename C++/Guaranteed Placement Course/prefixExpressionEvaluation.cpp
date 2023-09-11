fab28b23266765010bc6a610aff95a665fdcb407
Rashedul2001 <hacker360sr@gmail.com>
2023-09-12 01:34:28 +0600

    initial commit

---
+++ C++/Guaranteed Placement Course/prefixExpressionEvaluation.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <stack>
+#include <string>
+#include <cmath>
+int prefixEvaluation(const std::string &prefixExpression)
+{
+
+    std::stack<int> st;
+    for (int i = prefixExpression.length() - 1; i >= 0; i--)
+    {
+
+        if (prefixExpression[i] >= '0' && prefixExpression[i] <= '9')
+        {
+            st.push(prefixExpression[i] - '0');
+        }
+        else
+        {
+            int operand1 = st.top();
+            st.pop();
+            int operand2 = st.top();
+            st.pop();
+
+            switch (prefixExpression[i])
+            {
+            case '+':
+                st.push(operand1 + operand2);
+                break;
+            case '-':
+                st.push(operand1 - operand2);
+                break;
+            case '/':
+                st.push(operand1 / operand2);
+                break;
+            case '*':
+                st.push(operand1 * operand2);
+                break;
+            case '^':
+                st.push(std::pow(operand1, operand2));
+                break;
+            default:
+                std::cout << "Invalid Expression" << std::endl;
+                exit(1);
+
+                break;
+            }
+        }
+    }
+    return st.top();
+}
+
+int main()
+{
+    std::cout<<prefixEvaluation("-+7*45+20");
+
+
+    return 0;
+}
