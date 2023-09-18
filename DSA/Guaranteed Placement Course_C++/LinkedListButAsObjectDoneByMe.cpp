8b3dc0a677f298d5c6e562248da935f362a6fa7f
Rashedul2001 <hacker360sr@gmail.com>
2023-09-19 02:38:16 +0600

    clearing queues

---
+++ DSA/Guaranteed Placement Course_C++/LinkedListButAsObjectDoneByMe.cpp
@@ -0,0 +1,33 @@
+// #include <iostream>
+
+// class LinkedList
+// {
+// public:
+//     int value;
+//     LinkedList *next;
+//     LinkedList(int val) : next{nullptr}, value{val} {}
+// };
+
+// int main()
+// {
+
+//     LinkedList *list1 = nullptr;
+//     // make all the functions as a method of LinkedList object
+//     insertAtTail(list1, 25);
+//     insertAtTail(list1, 10);
+//     insertAtTail(list1, 30);
+
+//     display(list1);
+
+//     insertAtHead(list1, 20);
+//     insertAtHead(list1, 15);
+//     insertAtHead(list1, 5);
+
+//     display(list1);
+
+//     deleteNodeOf(list1, 15);
+
+//     display(list1);
+
+//     return 0;
+// }
--- DSA/Guaranteed Placement Course_C++/LinkedList_introductionAll.cpp
+++ DSA/Guaranteed Placement Course_C++/LinkedList_introductionAll.cpp
@@ -33,9 +33,14 @@ void insertAtHead(Node *&head, int val)
     n->next = head;
     head = n;
 }
-
+bool detectCycle(Node *head);
 void display(Node *head)
 {
+    if (detectCycle(head))
+    {
+        std::cout << "Can't print it's a cycle LinkedList" << std::endl;
+        return;
+    }
     Node *temp = head;
     while (temp != nullptr)
     {
@@ -91,23 +96,6 @@ void deleteNodeWithVal(Node *&head, int val)
     delete toDelete;
 }
 
-Node *reverseViaIterating(Node *&head)
-{
-
-    Node *prevPtr = nullptr;
-    Node *currPtr = head;
-    Node *nextPtr;
-
-    while (currPtr != nullptr)
-    {
-        nextPtr = currPtr->next;
-        currPtr->next = prevPtr;
-
-        prevPtr = currPtr;
-        currPtr = nextPtr;
-    }
-    return prevPtr;
-}
 Node *reverseViaRecursion(Node *&head)
 {
     if (head == nullptr || head->next == nullptr)
@@ -125,33 +113,167 @@ Node *reverseViaRecursion(Node *&head)
     // after doing this when we go back
     // recursively it will land on the earlier node which will be the head if there is one
 }
+Node *reverseViaIterating(Node *&head)
+{
+
+    Node *prevPtr = nullptr;
+    Node *currPtr = head;
+    Node *nextPtr;
+
+    while (currPtr != nullptr)
+    {
+        nextPtr = currPtr->next;
+        currPtr->next = prevPtr;
+
+        prevPtr = currPtr;
+        currPtr = nextPtr;
+    }
+    return prevPtr;
+}
+
+Node *reverseK_node(Node *&head, int k)
+{
+    Node *prevPtr = nullptr;
+    Node *currPtr = head;
+    Node *nextPtr;
+
+    int count = 0;
+    while (currPtr != nullptr && count < k)
+    {
+        nextPtr = currPtr->next;
+        currPtr->next = prevPtr;
+
+        prevPtr = currPtr;
+        currPtr = nextPtr;
+        count++;
+    }
+    if (currPtr != nullptr)
+    {
+        head->next = reverseK_node(nextPtr, k);
+    }
+
+    return prevPtr;
+}
+
+bool detectCycle(Node *head)
+{
+    Node *slowPtr = head;
+    Node *fastPtr = head;
+    while (fastPtr != nullptr && fastPtr->next != nullptr)
+    {
+        slowPtr = slowPtr->next;
+        fastPtr = fastPtr->next->next;
+        if (fastPtr == slowPtr)
+        {
+            // std::cout << "The Value:" << fastPtr->data << std::endl;
+            return true;
+        }
+    }
+    return false;
+}
+
+void makeCycle(Node *&head, int pos)
+{
+    Node *temp = head;
+    Node *repetitionNode;
+    int count = 1;
+    while (temp->next != nullptr)
+    {
+        if (count == pos)
+        {
+            repetitionNode = temp;
+        }
+        temp = temp->next;
+        count++;
+    }
+    temp->next = repetitionNode;
+}
+
+void removeCycle(Node *&head)
+{
+    Node *fastPtr = head;
+    Node *slowPtr = head;
+    Node *prevPtr = nullptr;
+
+    do
+    {
+        prevPtr = slowPtr;
+        fastPtr = fastPtr->next->next;
+        slowPtr = slowPtr->next;
+
+    } while (fastPtr != nullptr && fastPtr->next != nullptr && fastPtr != slowPtr);
+    // if there is no prevPtr and the additional if(slowPtr->next==nullptr)
+    //  this will not work if the fastPtr and the slowPtr are both at the head like 1, 2 3,then again 1 for this it won't work
+
+    if (fastPtr == nullptr || fastPtr->next == nullptr)
+    {
+        std::cout << "the LinkedList is not Cycle LinkedList" << std::endl;
+        return;
+    }
+    if (slowPtr == head)
+    {
+        prevPtr->next = nullptr;
+        return;
+    }
+
+    fastPtr = head;
+    while (fastPtr->next != slowPtr->next)
+    {
+        fastPtr = fastPtr->next;
+        slowPtr = slowPtr->next;
+    }
+    slowPtr->next = nullptr;
+}
 
 int main()
 {
 
     Node *linkedList1 = nullptr;
+    std::cout << "Inserting At Tail..." << std::endl;
     insertAtTail(linkedList1, 5);
     insertAtTail(linkedList1, 10);
     insertAtTail(linkedList1, 15);
     insertAtTail(linkedList1, 20);
     display(linkedList1);
+    std::cout << "Inserting At Head..." << std::endl;
     insertAtHead(linkedList1, 0);
     insertAtHead(linkedList1, -5);
     display(linkedList1);
-
+    std::cout << "Searching 20 in the Linked List..." << std::endl;
     search(linkedList1, 20);
-
+    std::cout << "Deleting Node with value 15..." << std::endl;
     deleteNodeWithVal(linkedList1, 15);
     display(linkedList1);
+    std::cout << "Deleting Node with value 50. Which doesn't exist..." << std::endl;
     deleteNodeWithVal(linkedList1, 50);
-    deleteNodeWithVal(linkedList1, -5);
     display(linkedList1);
-
+    std::cout << "reversing The Linked List by Iteration Method..." << std::endl;
     linkedList1 = reverseViaIterating(linkedList1);
     display(linkedList1);
-
+    std::cout << "reversing The Linked List by Recursion Method..." << std::endl;
     linkedList1 = reverseViaRecursion(linkedList1);
     display(linkedList1);
+    std::cout << "Reversing K-Nodes in the Linked List. Where K =2... " << std::endl;
+    linkedList1 = reverseK_node(linkedList1, 2);
+    display(linkedList1);
+
+    Node *cycleLinkedList = nullptr;
+    insertAtTail(cycleLinkedList, 5);
+    insertAtTail(cycleLinkedList, 10);
+    insertAtTail(cycleLinkedList, 6);
+    insertAtTail(cycleLinkedList, 25);
+    insertAtTail(cycleLinkedList, 2);
+    insertAtTail(cycleLinkedList, 9);
+    insertAtTail(cycleLinkedList, 40);
+    insertAtTail(cycleLinkedList, 55);
+    std::cout << "The LinkedList before making it Cycle LinkedList: " << std::endl;
+    display(cycleLinkedList);
+    makeCycle(cycleLinkedList, 1);
+    std::cout << "Detecting Cycle LinkedList" << std::endl;
+    std::cout << (detectCycle(cycleLinkedList) ? ("The linked List forms a cycle") : ("The linked List doesn't form a cycle")) << std::endl;
+
+    removeCycle(cycleLinkedList);
+    display(cycleLinkedList);
 
     return 0;
 }
