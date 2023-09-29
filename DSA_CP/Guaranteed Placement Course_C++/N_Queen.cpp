bb183c643a9ed661d590a20f8e7e113335cede49
Rashedul2001 <hacker360sr@gmail.com>
2023-09-29 18:08:51 +0600

    initial commit

--- DSA_CP/Guaranteed Placement Course_C++/N_Queen.cpp
+++ DSA_CP/Guaranteed Placement Course_C++/N_Queen.cpp
@@ -1,63 +1,85 @@
 #include <iostream>
-#include <vector>
+using namespace std;
 
-
-bool isSafe(std::vector<std::vector<int> > & mat,int x, int y) {
-    
-
-
-
-
-
-
-}
-
-bool nQueen(std::vector<std::vector<int>> &mat, int x)
+bool isSafe(int **arr, int x, int y, int n)
 {
-    if (x >= mat.size())
-        return true;
-    for (int col = 0; col < mat.size(); col++)
+    for (int row = 0; row < x; row++)
     {
-        if (isSafe(mat, x, col))
+        if (arr[row][y] == 1)
         {
-            mat[x][col] == 1;
-            if (nQueen(mat, ++x))
-            {
-                return true;
-            }
-            mat[x][col] = 0;
+            return false;
         }
     }
 
+    int row = x;
+    int col = y;
+    while (row >= 0 && col >= 0)
+    {
+        if (arr[row][col] == 1)
+        {
+            return false;
+        }
+        row--;
+        col--;
+    }
+    row = x;
+    col = y;
+    while (row >= 0 && col < n)
+    {
+        if (arr[row][col] == 1)
+        {
+            return false;
+        }
+        row--;
+        col++;
+    }
+    return true;
+}
+
+bool NQueen(int **arr, int x, int n)
+{
+    if (x >= n)
+    {
+        return true;
+    }
+    for (int col = 0; col < n; col++)
+    {
+
+        if (isSafe(arr, x, col, n))
+        {
+            arr[x][col] = 1;
+            if (NQueen(arr, x + 1, n))
+            {
+                return true;
+            }
+            arr[x][col] = 0;
+        }
+    }
     return false;
 }
 
 int main()
 {
-    std::cout << "Enter the size of the matrix: ";
     int n;
-    std::cin >> n;
-    std::vector<std::vector<int>> matrix(n, std::vector<int>(n, 0));
-    // for (auto row : matrix)
-    // {
-    //     for (auto col : row)
-    //     {
-    //         std::cout << col << ' ';
-    //     }
-    //     std::cout << std::endl;
-    // }
-    std::cout << matrix.size() << std::endl;
-    // every row have to have a queen
-    if (nQueen(matrix, 0)) // here 0 is the number of Queen has already been placed in the matrix
+    cin >> n;
+    int **arr = new int *[n];
+    for (int i = 0; i < n; i++)
     {
-
-        for (auto row : matrix)
+        arr[i] = new int[n];
+        for (int j = 0; j < n; j++)
         {
-            for (auto col : row)
+            arr[i][j] = 0;
+        }
+    }
+    if (NQueen(arr, 0, n))
+    {
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < n; j++)
             {
-                std::cout << col << ' ';
+                cout << arr[i][j] << " ";
             }
-            std::cout << std::endl;
+            cout << endl;
         }
     }
 
