b006d2ee3f8c77dd1f277e8e7e21cf584928e274
Rashedul2001 <hacker360sr@gmail.com>
2023-10-15 23:08:57 +0600

    finised

--- DSA_CPA/Guaranteed Placement Course_C++/stackUsingLinkedList.cpp
+++ DSA_CPA/Guaranteed Placement Course_C++/stackUsingLinkedList.cpp
@@ -12,13 +12,76 @@ public:
 class Stack
 {
 private:
-
-    
+    Node *stackTop;
+    int stackSize;
 
 public:
+    Stack() : stackTop{nullptr}, stackSize{0} {}
+    bool empty()
+    {
+        if (stackTop == nullptr)
+        {
+            return true;
+        }
+        else
+            return false;
+    }
+
+    void display()
+    {
+        if (empty())
+        {
+            std::cout << "Nothing to Show..." << std::endl;
+            return;
+        }
+        Node *temp = stackTop;
+        while (temp != nullptr)
+        {
+            std::cout << temp->data << " ";
+            temp = temp->next;
+        }
+        std::cout << std::endl;
+    }
+
     void push(int data)
     {
-        
+        Node *node = new Node(data);
+        node->next = stackTop;
+        stackTop = node;
+        stackSize++;
+    }
+    int top()
+    {
+        if (empty())
+        {
+            std::cerr << "The Stack is empty..." << std::endl;
+            return -1;
+        }
+        return stackTop->data;
+    }
+    void pop()
+    {
+        if (empty())
+        {
+            std::cout << "Underflow!" << std::endl;
+            return;
+        }
+
+        Node *toDelete = stackTop;
+        stackTop = stackTop->next;
+        delete toDelete;
+        stackSize--;
+    }
+    int size()
+    {
+        return this->stackSize;
+    }
+    ~Stack()
+    {
+        while (!empty())
+        {
+            pop();
+        }
     }
 };
 
@@ -26,7 +89,16 @@ int main()
 {
 
     Stack st;
-    st.push(50);
+    st.pop();
+    std::cout << st.top() << std::endl;
+    st.display();
+    st.push(560);
+    st.push(655);
+    st.display();
+    std::cout << st.size() << std::endl;
+    st.pop();
+    std::cout << st.size() << std::endl;
+    std::cout << st.top() << std::endl;
 
     return 0;
 }
