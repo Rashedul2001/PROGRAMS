bf585c7a164888053160fc53c283eb883406ebfa
Rashedul2001 <hacker360sr@gmail.com>
2023-10-19 19:23:03 +0600

    First Commit

--- DSA_CPA/Guaranteed Placement Course_C++/stockSpanProblem.cpp
+++ DSA_CPA/Guaranteed Placement Course_C++/stockSpanProblem.cpp
@@ -1,32 +1,27 @@
-#include<iostream>
-#include<vector>
-#include<stack>
+#include <iostream>
+#include <vector>
+#include <stack>
 
-
-
-int main(){
-    std::vector<int> stocks{100,80,60,70,65,75,85};// answer is for consecutive days not just the total number of days
+int main()
+{
+    std::vector<int> stocks{100, 80, 60, 70, 65, 75, 85}; // answer is for consecutive days not just the total number of days
     std::vector<int> result;
 
-    std::stack<std::pair<int,int> > st;
-    for(auto stock: stocks){
-        int days=1;
-        while(!st.empty() and st.top().first<=stock){
-            days+=st.top().second;
+    std::stack<std::pair<int, int>> st;
+    for (auto stock : stocks)
+    {
+        int days = 1;
+        while (!st.empty() and st.top().first <= stock)
+        {
+            days += st.top().second;
             st.pop();
         }
-        st.push({stock,days});
+        st.push({stock, days});
         result.push_back(days);
     }
 
-
-
-    for(auto re: result)
+    for (auto re : result)
         std::cout << re << ' ';
-
-
-
-
 
     return 0;
 }
