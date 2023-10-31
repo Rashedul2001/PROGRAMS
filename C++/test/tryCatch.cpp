dd6dc9744b513dc47854f0b974991277b9e35b0d
Rashedul2001 <hacker360sr@gmail.com>
2023-10-31 14:24:33 +0600

    First Commit

--- C++/test/tryCatch.cpp
+++ C++/test/tryCatch.cpp
@@ -1,29 +1,24 @@
-#include<iostream>
-#include<stdexcept>
+#include <iostream>
+#include <stdexcept>
 
-int main(){
-    std::cout<< "Enter two numbers to Divide: ";
-    double num1,num2;
+int main()
+{
+    std::cout << "Enter two numbers to Divide: ";
+    double num1, num2;
     std::cin >> num1 >> num2;
-    try{
-        if(num2==0){
+    try
+    {
+        if (num2 == 0)
+        {
             // throw std::runtime_error("Cannot divide by Zero.");
         }
-        double result= num1/num2;
-        std::cout << "The result is: "<< result <<std::endl;
-
-
-
-
+        double result = num1 / num2;
+        std::cout << "The result is: " << result << std::endl;
     }
-    catch(std::exception& e){
-        std::cerr<<"Error "<< e.what() << std::endl; 
-
+    catch (std::exception &e)
+    {
+        std::cerr << "Error " << e.what() << std::endl;
     }
-
-
-
-
 
     return 0;
 }
