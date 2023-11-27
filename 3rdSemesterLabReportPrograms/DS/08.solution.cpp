f70878c860d47b504b8eb083dad93091425c7553
Rashedul2001 <hacker360sr@gmail.com>
2023-11-28 01:53:58 +0600

    added a line

--- 3rdSemesterLabReportPrograms/DS/08.solution.cpp
+++ 3rdSemesterLabReportPrograms/DS/08.solution.cpp
@@ -4,6 +4,9 @@ int main()
 {
     std::string mainStr = "This program is not Error Free";
     std::string toRem = " not";
+    std::cout << "Before Deleting the substring the Text is:" << std::endl;
+    std::cout << mainStr << std::endl;
+
     size_t pos;
     while ((pos = mainStr.find(toRem)) != std::string::npos)
     {
