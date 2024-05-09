95bd3e550f786f8ddd94109833281c5c1e1dd92f
Rashedul2001 <hacker360sr@gmail.com>
2024-05-09 22:37:13 +0600

    deleted codeFiles

--- CONTEST_CodeForces_Folder/codeJava.java
+++
@@ -1,9 +0,0 @@
-import java.util.Scanner;
-
-public class codeJava {
-    public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-
-        System.out.println("Write Your code Here");
-    }
-}
