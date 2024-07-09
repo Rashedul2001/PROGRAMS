d7c188156c76f9ee041d39f3570524b198a2191c
Rashedul2001 <hacker360sr@gmail.com>
2024-07-09 18:15:13 +0600

    Complete

--- AndroidStudioProjects/ToDoList/app/src/main/java/com/rashedul/todolist/FileHelper.java
+++ AndroidStudioProjects/ToDoList/app/src/main/java/com/rashedul/todolist/FileHelper.java
@@ -2,15 +2,43 @@ package com.rashedul.todolist;
 
 import android.content.Context;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.util.ArrayList;
 
 public class FileHelper {
     public static String fileName = "listInfo.dat";
 
-    public static void writeData(ArrayList<String> item, Context context)
-    {
-        FileOutputStream fos = context.openFileOutput(fileName,Context.MODE_PRIVATE);
-        
+    public static void writeData(ArrayList<String> item, Context context) {
+        try {
+            FileOutputStream fos = context.openFileOutput(fileName, Context.MODE_PRIVATE);
+            ObjectOutputStream oos = new ObjectOutputStream(fos);
+            oos.writeObject(item);
+            oos.close();
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
+
+    public static ArrayList<String> readData(Context context) {
+        ArrayList<String> itemList = null;
+        try {
+            FileInputStream fis = context.openFileInput(fileName);
+            ObjectInputStream ois = new ObjectInputStream(fis);
+            itemList = (ArrayList<String>) ois.readObject();
+        } catch (FileNotFoundException e) {
+            itemList = new ArrayList<>();
+        } catch (IOException | ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+        return itemList;
+
+    }
+
+
 }
--- AndroidStudioProjects/ToDoList/app/src/main/java/com/rashedul/todolist/MainActivity.java
+++ AndroidStudioProjects/ToDoList/app/src/main/java/com/rashedul/todolist/MainActivity.java
@@ -1,12 +1,15 @@
 package com.rashedul.todolist;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
 
 import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
@@ -32,11 +35,41 @@ public class MainActivity extends AppCompatActivity {
             return insets;
         });
         editText = findViewById(R.id.editText);
-        add = findViewById( R.id.buttonEnter);
+        add = findViewById(R.id.buttonEnter);
         list = findViewById(R.id.list);
+        itemList = FileHelper.readData(this);
+        arrayAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, itemList);
+        list.setAdapter(arrayAdapter);
+        add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String item = editText.getText().toString();
+                if (item.isEmpty()) {
+                    return;
+                }
+                itemList.add(item);
+                editText.setText("");
+                FileHelper.writeData(itemList, getApplicationContext());
+                arrayAdapter.notifyDataSetChanged();
+            }
+        });
+        list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                AlertDialog.Builder alert = new AlertDialog.Builder(MainActivity.this);
+                alert.setTitle("Delete");
+                alert.setMessage("Are you sure you want to delete this item?");
+                alert.setCancelable(false);
+                alert.setPositiveButton("Yes", (dialog, which) -> {
+                    itemList.remove(position);
+                    arrayAdapter.notifyDataSetChanged();
+                    FileHelper.writeData(itemList, getApplicationContext());
+                });
+                alert.setNegativeButton("No", null);
+                AlertDialog alertDialog = alert.create();
+                alertDialog.show();
+            }
 
-
-
-
+        });
     }
 }
