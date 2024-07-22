e970d3b014ce1ab2d329c5427114ed897a1895f6
Rashedul2001 <hacker360sr@gmail.com>
2024-07-22 22:12:53 +0600

    format it

--- DSA_CPA/Greedy_Algorithms/highestMultiplicationOfThreeNumber.cpp
+++ DSA_CPA/Greedy_Algorithms/highestMultiplicationOfThreeNumber.cpp
@@ -1,31 +1,28 @@
-#include<iostream>
-#include<algorithm>
+#include <iostream>
+#include <algorithm>
 using namespace std;
 
-int main(){
+int main()
+{
 
     int n;
-    cin>>n;
+    cin >> n;
     int arr[n];
-    for(int i=0;i<n;i++){
-        cin>>arr[i];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
     }
-    sort(arr,arr+n); 
+    sort(arr, arr + n);
 
     // for(int i=0;i<n;i++){
     //     cout<<arr[i]<<' ';
     // }
 
+    int firstMax = 0, secondMax = 0;
 
-    int firstMax=0,secondMax=0;
-
-    firstMax=arr[0]*arr[1]*arr[n-1];
-    secondMax=arr[n-1]*arr[n-2]*arr[n-3];
-    cout<<max(firstMax,secondMax)<<endl;
-
-
-
-
+    firstMax = arr[0] * arr[1] * arr[n - 1];
+    secondMax = arr[n - 1] * arr[n - 2] * arr[n - 3];
+    cout << max(firstMax, secondMax) << endl;
 
     return 0;
 }
