77f3c773124fb8ab4abc37317857ef97ead47452
Rashedul2001 <hacker360sr@gmail.com>
2024-08-12 12:24:11 +0600

    Calculator App notComplete IwCMO

--- AndroidStudioProjects/Calculator/app/src/main/java/com/rashedul/calculator/MainActivity.java
+++ AndroidStudioProjects/Calculator/app/src/main/java/com/rashedul/calculator/MainActivity.java
@@ -1,6 +1,9 @@
 package com.rashedul.calculator;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +12,12 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+    TextView calculation, result;
+    Button buttonAc, buttonDel, buttonMul, buttonDivide, buttonPlus, buttonMinus, button0, button1, button2, button3, button4, button5;
+    Button button6, button7, button8, button9, buttonDot, buttonEqual;
+    String currentNumber = null;
+    double firstNumber = 0.0,secondNumber = 0.0;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +29,133 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        calculation = findViewById(R.id.textViewMethods);
+        result = findViewById(R.id.textViewResult);
+
+        button0 = findViewById(R.id.buttonZero);
+        button1 = findViewById(R.id.buttonOne);
+        button2 = findViewById(R.id.buttonTwo);
+        button3 = findViewById(R.id.buttonThree);
+        button4 = findViewById(R.id.buttonFour);
+        button5 = findViewById(R.id.buttonFive);
+        button6 = findViewById(R.id.buttonSix);
+        button7 = findViewById(R.id.buttonSeven);
+        button8 = findViewById(R.id.buttonEight);
+        button9 = findViewById(R.id.buttonNine);
+
+        buttonAc = findViewById(R.id.buttonAC);
+        buttonDel = findViewById(R.id.buttonDel);
+        buttonDivide = findViewById(R.id.buttonDivide);
+        buttonDot = findViewById(R.id.buttonDot);
+        buttonMinus = findViewById(R.id.buttonMinus);
+        buttonMul = findViewById(R.id.buttonMul);
+        buttonPlus = findViewById(R.id.buttonPlus);
+        buttonEqual = findViewById(R.id.buttonEqual);
+
+        button0.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                numberClick("0");
+            }
+        });
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                numberClick("1");
+            }
+        });
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                numberClick("2");
+            }
+        });
+        button3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                numberClick("3");
+            }
+        });
+        button4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                numberClick("4");
+            }
+        });
+        button5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                numberClick("5");
+            }
+        });
+        button6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                numberClick("9");
+            }
+        });
+        button7.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                numberClick("9");
+            }
+        });
+        button8.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                numberClick("8");
+            }
+        });
+        button9.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                numberClick("9");
+            }
+        });
+        buttonAc.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+            }
+        });
+        buttonDot.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+            }
+        });
+        buttonDel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+            }
+        });
+        buttonDivide.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+            }
+        });
+        buttonMul.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+            }
+        });
+        buttonEqual.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+            }
+        });
+        buttonMinus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+            }
+        });
+    }
+
+    void numberClick(String input) {
+        if (currentNumber == null) {
+            currentNumber = input;
+        } else {
+            currentNumber += input;
+        }
+        calculation.setText(currentNumber);
     }
 }
