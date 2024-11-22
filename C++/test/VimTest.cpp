14adc9a9454d8034f3dc81b7ac1ce83f9a7625db
Rashedul2001 <hacker360sr@gmail.com>
2024-11-22 13:34:06 +0600

    All Changes included

--- C++/test/VimTest.cpp
+++ C++/test/VimTest.cpp
@@ -4,15 +4,11 @@
 
 int main()
 {
-    std::vector v{ 0, 1, 2 };
-    v.resize(5); // set size to 5
-
-    std::cout << "The length is: " << v.size() << '\n';
-
-    for (int i : v)
-        std::cout << i << ' ';
-
-    std::cout << '\n';
+    const vector<int> vec{8,39,23};
+    
+    for (auto it: vec){
+    	cout << it ;
+	}
 
     return 0;
 }
